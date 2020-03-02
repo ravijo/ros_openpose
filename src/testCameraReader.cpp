98e928c883474eace8c71f588b74bf25666ee9ca
Ravi Joshi <ravi2008joshi@gmail.com>
2020-03-02 23:52:06 +0900

    test file for camera reader

--- src/testCameraReader.cpp
+++ src/testCameraReader.cpp
@@ -5,8 +5,6 @@
 */
 
 #include <iostream>
-#include <vector>
-#include <memory>
 #include <opencv2/highgui/highgui.hpp>
 #include <ros_openpose/cameraReader.hpp>
 
@@ -21,27 +19,19 @@ void show(sPtrCameraReader readers)
     auto depthImage = readers->getDepthFrame();
 
     if (!colorImage.empty())
-    {
       cv::imshow("color image", colorImage);
-    }
     else
-    {
       // display the error at most once per 10 seconds
       ROS_WARN_THROTTLE(10, "Empty color image frame detected. Ignoring...");
-    }
 
     if (!depthImage.empty())
-    {
       cv::imshow("depth image", depthImage);
-    }
     else
-    {
       // display the error at most once per 10 seconds
       ROS_WARN_THROTTLE(10, "Empty depth image frame detected. Ignoring...");
-    }
 
     int key = cv::waitKey(1) & 255;
-    if (key == 27)  // Escape key
+    if (key == 27)  // escape key
       break;
 
     ros::spinOnce();
@@ -55,8 +45,8 @@ int main(int argc, char* argv[])
   ros::NodeHandle nh;
 
   const std::string colorTopic = "/camera/color/image_raw";
-  const std::string depthTopic = "/camera/aligned_depth_to_color/image_raw";
   const std::string camInfoTopic = "/camera/color/camera_info";
+  const std::string depthTopic = "/camera/aligned_depth_to_color/image_raw";
   const auto cameraReader = std::make_shared<ros_openpose::CameraReader>(nh, colorTopic, depthTopic, camInfoTopic);
 
   show(cameraReader);
